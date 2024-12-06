fd88a903c958c49e11fe29a2ae6871cd1e556d70
raffeekk <idmitryfrl@gmail.com>
2024-12-06 20:26:50 +0300

    init application

---
+++ web/src/test/java/so/team/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package so.team.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
